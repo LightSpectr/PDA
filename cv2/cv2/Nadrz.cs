0e83212202f86b581698dd438eb1db9470f6d755
LightSpectr <maked440@gmail.com>
2022-10-11 11:52:40 +0200

    data type of values int->uint; ++Pridat method

--- cv2/cv2/Nadrz.cs
+++ cv2/cv2/Nadrz.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using System.Runtime.Serialization;
 
 namespace cv2
 {
     internal class Nadrz
     {
+        private uint stav;
+        private readonly uint kapacita;
+
+        public Nadrz(uint kapacita)
+        {
+            this.stav = 0;
+            this.kapacita = kapacita;
+        }
+
+        public uint Stav
+        {
+            get { return stav; }
+            set { stav = value; }
+        }
+
+        public void Pridat(uint val)
+        {
+            if (val + stav <= kapacita)
+            {
+                stav += val;
+            }
+            else
+            {
+                throw new NadrzNelzeProvestOperaciException("Capacity overflow");
+            }
+        }
+
+        public void Odebrat(uint val)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "placeholder";
+        }
+
     }
+
+    public class NadrzNelzeProvestOperaciException : Exception
+    {
+       
+        public NadrzNelzeProvestOperaciException(string message)
+            : base(message)
+        { }
+
+        
+    }
+
 }
