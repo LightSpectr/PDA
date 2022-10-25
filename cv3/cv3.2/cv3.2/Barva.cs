1f7b97ead5c8e0fd8c94164493ee4ba285eb1987
LightSpectr <maked440@gmail.com>
2022-10-25 10:58:11 +0200

    barva class

---
+++ cv3/cv3.2/cv3.2/Barva.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+
+namespace cv3._2
+{
+    internal class Barva
+    {
+        private readonly byte red, green, blue;
+        private readonly string name;
+        private readonly double intensity;
+
+        public Barva(string name, byte red, byte green, byte blue)
+        {
+            this.name = name;
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            intensity = (this.red + this.green + this.blue) / 3;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+        public double Intensity
+        {
+            get { return this.intensity; }  
+        }
+
+        public string Hex
+        {
+            get { return String.Format("#{0}{1}{2}", this.red, this.green, this.blue); }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} (#{1}{2}{3}):{4})", name, red.ToString("X"), green.ToString("X"), blue.ToString("X"), intensity) ;
+        }
+
+    }
+}
