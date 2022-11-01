15d44305802276df8dcc7083367e6d1f55bb29b8
LightSpectr <maked440@gmail.com>
2022-11-01 12:13:15 +0100

    Comparers++

--- cv3/cv3.2/cv3.2/Barva.cs
+++ cv3/cv3.2/cv3.2/Barva.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Xml.Linq;
+using System.Collections;
 
 namespace cv3._2
 {
@@ -42,5 +43,45 @@ namespace cv3._2
             return String.Format("{0} (#{1}{2}{3}):{4}", name, red.ToString("X2"), green.ToString("X2"), blue.ToString("X2"), intensity) ;
         }
 
+    }
+    class BarvaComparerAsc : IComparer
+    {
+
+        int IComparer.Compare(Object x, Object y)
+        {
+            if (x == null)
+                return (y == null) ? 0 : 1;
+
+            if (y == null)
+                return -1;
+
+            Barva p1 = (Barva)x;
+            Barva p2 = (Barva)y;
+
+            // Uncomment this to sort by Name 
+            // return( (new CaseInsensitiveComparer()).Compare( p1.Name, p2.Name) );
+
+            return Convert.ToInt32(p1.Intensity - p2.Intensity);
+        }
+    }
+    class BarvaComparerDesc : IComparer
+    {
+
+        int IComparer.Compare(Object x, Object y)
+        {
+            if (x == null)
+                return (y == null) ? 0 : 1;
+
+            if (y == null)
+                return -1;
+
+            Barva p1 = (Barva)x;
+            Barva p2 = (Barva)y;
+
+            // Uncomment this to sort by Name 
+            // return( (new CaseInsensitiveComparer()).Compare( p1.Name, p2.Name) );
+
+            return Convert.ToInt32(p2.Intensity - p1.Intensity);
+        }
     }
 }
