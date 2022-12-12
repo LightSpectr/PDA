4d4282dcbdeb55c0bf0df89e73e8d0782dfca65a
LightSpectr <maked440@gmail.com>
2022-12-12 21:00:43 +0100

    cv 3.1 done

--- cv3/cv3.1/cv3.1/Form1.Designer.cs
+++ cv3/cv3.1/cv3.1/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_calc = new System.Windows.Forms.Button();
+            this.textBox_tax = new System.Windows.Forms.TextBox();
+            this.textBox_income = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_calc
+            // 
+            this.button_calc.Location = new System.Drawing.Point(12, 76);
+            this.button_calc.Name = "button_calc";
+            this.button_calc.Size = new System.Drawing.Size(184, 23);
+            this.button_calc.TabIndex = 0;
+            this.button_calc.Text = "Výpočet";
+            this.button_calc.UseVisualStyleBackColor = true;
+            this.button_calc.Click += new System.EventHandler(this.button_calc_Click);
+            // 
+            // textBox_tax
+            // 
+            this.textBox_tax.Location = new System.Drawing.Point(96, 6);
+            this.textBox_tax.Name = "textBox_tax";
+            this.textBox_tax.Size = new System.Drawing.Size(100, 23);
+            this.textBox_tax.TabIndex = 1;
+            // 
+            // textBox_income
+            // 
+            this.textBox_income.Location = new System.Drawing.Point(96, 38);
+            this.textBox_income.Name = "textBox_income";
+            this.textBox_income.Size = new System.Drawing.Size(100, 23);
+            this.textBox_income.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sazba daně";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "mzda (hrubá)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(207, 109);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_income);
+            this.Controls.Add(this.textBox_tax);
+            this.Controls.Add(this.button_calc);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button_calc;
+        private TextBox textBox_tax;
+        private TextBox textBox_income;
+        private Label label1;
+        private Label label2;
     }
 }
--- cv3/cv3.1/cv3.1/Form1.cs
+++ cv3/cv3.1/cv3.1/Form1.cs
@@ -1,10 +1,50 @@
-namespace cv3._1
+﻿namespace cv3._1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_calc_Click(object sender, EventArgs e)
+        {
+            double income = 0;
+            double tax = 0;
+            double income_tax = 0;
+            try
+            {
+                tax = Double.Parse(textBox_tax.Text);
+                if (tax < 0 || tax > 100)
+                {
+                    MessageBox.Show("Tax must be 0-100 %", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    return;
+                }
+                income = Double.Parse(textBox_income.Text);
+                if (income < 0)
+                {
+                    MessageBox.Show("Income can't be negative", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    return;
+                }
+                income_tax = (tax * income) / 100.0;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Input string is not a sequence of digits.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+               
+                return;
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("The number cannot fit in an Int32.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+ 
+                return;
+            }
+            string text = string.Format("Sazba daně: {0}%\n mzda (hrubá): {1}\n mzda (čistá): {2} ", textBox_tax.Text, textBox_income.Text, income - income_tax);
+
+            MessageBox.Show(text, "Výpočet", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
